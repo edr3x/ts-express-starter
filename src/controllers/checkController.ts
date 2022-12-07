378a2eec20faa511f7dfc5fd4daa3fba99e05ebc
edr3x <dhunganaannanta@gmail.com>
2022-12-07 20:28:11 +0545

    prettier config

--- src/controllers/checkController.ts
+++ src/controllers/checkController.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from 'express'
 
-import * as CheckService from "../services/checkServices"
+import * as CheckService from '../services/checkServices'
 
 export const testController = async (_req: Request, res: Response) => {
     try {
-        const test = await CheckService.checkRoute();
+        const test = await CheckService.checkRoute()
         return res.status(200).json({ test })
     } catch (e: any) {
-        return res.status(500).json(e.message);
+        return res.status(500).json(e.message)
     }
 }
-
